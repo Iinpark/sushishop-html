a9516c0f753c8a3f45df73d2b02899ff4fab14e1
Sultan Esbolatov <linpark11197@gmail.com>
2020-07-08 18:30:16 +0600

    добавили боковое меню

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,23 @@
+$('body').css('overflow', 'visible');
+
 $('.phone-dropdown-icon').click(() => {
   $('.phone-secondary').toggleClass('open');
+});
+
+// handleing menu clicking
+const menu = (index, value) => {
+  console.log('before', value);
+  if (value === 'hidden') {
+    $('body').css('overflow', 'visible');
+  }
+  if (value === 'visible') {
+    $('body').css('overflow', 'hidden');
+  }
+};
+
+$('.menu-toggle').on('click', function (e) {
+  e.preventDefault();
+  $('.menu').toggleClass('menu_active');
+  $('.content').toggleClass('content_active');
+  $('body').css('overflow', menu);
 });
